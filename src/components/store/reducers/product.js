1bf1995a824a8be75f49b21aa6e50c1da9fb286e
Liza-web-felix <lizfelix.ti@outlook.com>
2022-08-18 22:21:14 -0300

    projeto dio store adicionado

---
+++ src/components/store/reducers/product.js
@@ -0,0 +1,136 @@
+const productsList = [
+    {
+      id_product: 1,
+      category: "Notebooks Dell",
+      fk_idcategorys: 1,
+      name_product: "Dell Vostro",
+      price: 3900,
+      image: "./images/produtos/Dell-Vostro.jpg",
+      id_categorys: 1,
+      name_categorys: "Dell",
+      selected: false
+    },
+    {
+      id_product: 5,
+      category: "Notebooks Dell",
+      fk_idcategorys: 1,
+      name_product: "Dell Inspiron",
+      price: 5000,
+      image: "./images/produtos/Dell-Inspiron.jpg",
+      id_categorys: 1,
+      name_categorys: "Dell",
+      selected: false
+    },
+    {
+      id_product: 8,
+      category: "Notebooks Dell",
+      fk_idcategorys: 1,
+      name_product: "Dell Gamer",
+      price: 5800,
+      image: "./images/produtos/Dell-Gamer.jpg",
+      id_categorys: 1,
+      name_categorys: "Dell",
+      selected: false
+    },
+    {
+      id_product: 4,
+      category: "Notebooks Lenovo",
+      fk_idcategorys: 2,
+      name_product: "Lenovo Ideapad S145",
+      price: 5300,
+      image: "./images/produtos/Lenovo-Ideapad-S145.jpg",
+      id_categorys: 2,
+      name_categorys: "Lenovo-Ideapad",
+      selected: false
+    },
+    {
+      id_product: 10,
+      category: "Notebooks Lenovo",
+      fk_idcategorys: 2,
+      name_product: "Lenovo Ideapad Gaming",
+      price: 350.99,
+      image: "./images/produtos/Lenovo-Ideapad-Gaming-15IMH.jpg",
+      id_categorys: 2,
+      name_categorys: "Lenovo-Ideapad",
+      selected: false
+    },
+    {
+      id_product: 11,
+      category: "Notebooks Lenovo",
+      fk_idcategorys: 3,
+      name_product: "Lenovo ThinkPad L440",
+      price: 3600,
+      image: "./images/produtos/Lenovo-ThinkPad-L440.jpg",
+      id_categorys: 3,
+      name_categorys: "Lenovo-ThinkPad",
+      selected: false
+    },
+    {
+      id_product: 6,
+      category: "Mouses Lenovo",
+      fk_idcategorys: 4,
+      name_product: "Lenovo Mouse ThinkPad",
+      price: 220,
+      image: "./images/produtos/Lenovo-ThinkPad-Bluetooth-Mouse-1200DPI.jpg",
+      id_categorys: 4,
+      name_categorys: "Mouse-ThinkPad",
+      selected: false
+    },
+    {
+      id_product: 7,
+      category: "Mouses Lenovo",
+      fk_idcategorys: 5,
+      name_product: "Lenovo Mouse Rato 530",
+      price: 190,
+      image: "./images/produtos/Lenovo-Mouse-Rato-530.jpg",
+      id_categorys: 5,
+      name_categorys: "Mouse-Rato",
+      selected: false
+    },
+    {
+      id_product: 3,
+      category: "Fones",
+      fk_idcategorys: 7,
+      name_product: "Lenovo Headset",
+      price: 500,
+      image: "./images/produtos/Lenovo-Headset-P320-Preto.jpg",
+      id_categorys: 7,
+      name_categorys: "Fone-Headset",
+      selected: false
+    },
+    {
+      id_product: 13,
+      category: "Fones",
+      fk_idcategorys: 8,
+      name_product: "Lenovo Headphone",
+      price: 250,
+      image: "./images/produtos/Lenovo-Headphone-Preto-e-Vermelho.jpg",
+      id_categorys: 8,
+      name_categorys: "Fone-Headphone",
+      selected: false
+    },
+    {
+      id_product: 8,
+      category: "Kindle",
+      fk_idcategorys: 12,
+      name_product: "Kindle 8°",
+      price: 257,
+      image: "./images/produtos/Kindle-8-Geracao.jpg",
+      id_categorys: 12,
+      name_categorys: "Kindle DIO",
+      selected: false
+    },
+    {
+      id_product: 15,
+      category: "Kindle",
+      fk_idcategorys: 12,
+      name_product: "E-reader Kindle 10°",
+      price: 690,
+      image: "./images/produtos/Kindle-E-reader-10-Geracao.jpg",
+      id_categorys: 12,
+      name_categorys: "Kindle DIO",
+      selected: false
+    },
+];
+
+export default productsList;
